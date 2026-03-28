fcc8d4eced9b46488777deb1355642a785425772
Sumit Kumar Pandey <sumit@opskube.com>
2026-03-28 17:21:15 +0530

    added the api/v1 in the top param

--- src/main/java/com/flyway/airport/AirportController.java
+++ src/main/java/com/flyway/airport/AirportController.java
@@ -6,6 +6,7 @@ import com.flyway.common.ApiResponse;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/v1")
 public class AirportController {
 
     private final AirportService airportService;
@@ -21,7 +23,7 @@ public class AirportController {
         this.airportService = airportService;
     }
 
-    @GetMapping("/api/airports/search")
+    @GetMapping("/airports/search")
     public ResponseEntity<ApiResponse<List<AirportResponse>>> searchAirports(
             @RequestParam String keyword
     ) {
