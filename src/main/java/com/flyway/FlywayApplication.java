89bb67319705e773856266a6f28cf329ea3d09dd
Sumit Kumar Pandey <officialsumitpandey@gmail.com>
2026-04-12 22:37:47 +0530

    fixed the timezone and the keyword param issue

--- src/main/java/com/flyway/FlywayApplication.java
+++ src/main/java/com/flyway/FlywayApplication.java
@@ -1,5 +1,7 @@
 package com.flyway;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FlywayApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(FlywayApplication.class, args);
 	}
 
